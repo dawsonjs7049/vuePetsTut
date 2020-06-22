50e856062177221700cd2dd3149565f869dc4b1e
JakeTheCodingSnake <jake906@charter.net>
2020-06-22 15:18:55 -0500

    first commit

---
+++ src/data/dogs.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: 'Sheeba',
+        breed: 'collie',
+        gender: 'female',
+        age: '7',
+        color: 'black/white',
+        weight: 34,
+        location: 'fourside',
+        notes: 'Pure breed. Trained for competitions.'
+    },
+    {
+        name: 'Zeus',
+        breed: 'Springer Spaniel',
+        gender: 'male',
+        age: '6',
+        color: 'black/white',
+        weight: 55,
+        location: 'Eau Claire',
+        notes: 'Pure breed. Trained for hunting.'
+    },
+    {
+        name: 'Spot',
+        breed: 'Black Lab',
+        gender: 'male',
+        age: '9',
+        color: 'black',
+        weight: 67,
+        location: 'Madison',
+        notes: 'Pure breed. Loves people.'
+    }
+]
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import state from './state';
+import mutations from './mutations';
+import actions from './actions';
+import getters from './getters';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  state,
+  mutations,
+  actions,
+  getters,
 });
+
+/*
+actions are async, they call mutations which actually update state. State is
+linked to computed properties so they will automatically update upon mutation call.
+Getters are something that retrieves state and can modify it on fetch for a component,
+but it doesn't actually change the state (only for that component's use)
+*/
